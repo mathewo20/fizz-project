77dba214127328a2371be00af2ae7b532a5ee110
mathewo20 <mo237704937@gmail.com>
2025-12-10 14:07:49 +0000

    think outside the box

---
+++ main.py
@@ -0,0 +1,17 @@
+print("FizzBuzzproject")
+
+number = 100
+second = 1
+
+for second in range(number):
+    num = number - second
+    fizz = number /3
+    buzz = number /5
+    fizzbuzz = number /3 and number /5
+
+    if (fizz is not fraction):
+        print("fizz")
+
+
+print(num)
+
